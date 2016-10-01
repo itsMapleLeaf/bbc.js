87c9a8bd5f0b379ae220deaba414cc724f667024
Kingdaro <kingdaro@gmail.com>
2016-10-01 03:21:43 -0400

    rename the output file a little more intuitive for simple browser usage

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel'
 
 export default {
   entry: 'src/index.js',
-  dest: 'dist/index.js',
+  dest: 'dist/bbc.js',
   format: 'iife',
   moduleName: 'BBC',
   plugins: [ babel() ],
