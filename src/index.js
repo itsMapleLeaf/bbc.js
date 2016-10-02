be6eaabc87f75e91c2d50d3e91194ea8a628455d
Kingdaro <kingdaro@gmail.com>
2016-10-02 08:06:58 -0400

    rewrite

--- src/index.js
+++ src/index.js
@@ -1,41 +1,49 @@
-const defaultTags = {
-  b: { start: '<strong>', end: '</strong>' },
-  i: { start: '<em>', end: '</em>' },
-  u: { start: '<u>', end: '</u>' },
-  url: { start: (_, url) => `<a href="${url}">`, end: '</a>' },
-  color: { start: (_, color) => `<span style="color: ${color}">`, end: '</span>' },
-  nobbc: { nobbc: true },
-}
-
-function parser(tags = defaultTags) {
-  const nobbcExp = new RegExp(Object.keys(tags)
-    .filter(tag => tags[tag].nobbc)
-    .map(tag => `\\[${tag}(?:=.*?)?]|\\[\\/${tag}]`)
-    .join('|'))
-
-  const tagExpressions = {}
-  Object.keys(tags).forEach(tag => {
-    tagExpressions[tag] = {
-      startexp: new RegExp(`\\[${tag}(?:=(.*?))?]`, 'i'),
-      endexp: new RegExp(`\\[\\/${tag}]`, 'i'),
-    }
-  })
-
-  return input => {
-    const regions = input.split(nobbcExp)
-
-    for (let i = 0; i < regions.length; i += 2) {
-      for (const tag in tags) {
-        const { start = '', end = '' } = tags[tag]
-        const { startexp, endexp } = tagExpressions[tag]
-        while (startexp.test(regions[i]) && endexp.test(regions[i])) {
-          regions[i] = regions[i].replace(startexp, start).replace(endexp, end)
-        }
-      }
-    }
-
-    return regions.join('')
-  }
+function parser() {
+  // return new parser function
 }
 
 export { parser }
+
+// const defaultTags = {
+//   b: { start: '<strong>', end: '</strong>' },
+//   i: { start: '<em>', end: '</em>' },
+//   u: { start: '<u>', end: '</u>' },
+//   url: { start: (_, url) => `<a href="${url}">`, end: '</a>' },
+//   color: { start: (_, color) => `<span style="color: ${color}">`, end: '</span>' },
+//   nobbc: { nobbc: true },
+// }
+
+// function parser(tags) {
+//   const nobbcExp = new RegExp(Object.keys(tags)
+//     .filter(tag => tags[tag].nobbc)
+//     .map(tag => `\\[${tag}(?:=.*?)?]|\\[\\/${tag}]`)
+//     .join('|'))
+//
+//   const tagExpressions = {}
+//   Object.keys(tags).forEach(tag => {
+//     tagExpressions[tag] = {
+//       startexp: new RegExp(`\\[${tag}(?:=(.*?))?]`, 'i'),
+//       endexp: new RegExp(`\\[\\/${tag}]`, 'i'),
+//     }
+//   })
+//
+//   return input => {
+//     const regions = input.split(nobbcExp)
+//
+//     for (let i = 0; i < regions.length; i += 2) {
+//       for (const tag in tags) {
+//         const { start = '', end = '' } = tags[tag]
+//         const { startexp, endexp } = tagExpressions[tag]
+//         while (startexp.test(regions[i]) && endexp.test(regions[i])) {
+//           regions[i] = regions[i]
+//             .replace(startexp, start)
+//             .replace(endexp, end)
+//         }
+//       }
+//     }
+//
+//     return regions.join('')
+//   }
+// }
+//
+// export { parser }
