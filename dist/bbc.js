0ae2aa5d6c1d6ec0c5263ba775d30dff294972e8
Kingdaro <kingdaro@gmail.com>
2016-10-02 13:06:11 -0400

    fixed bugs

--- dist/bbc.js
+++ dist/bbc.js
@@ -60,72 +60,60 @@ function getTokens(input) {
   }
 }
 
-function createNodeList(tokens) {
+function createNode(tokens) {
   var pos = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
   var nodes = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
   var currentTag = arguments[3];
 
-  if (pos >= tokens.length - 1) {
-    return nodes;
+  if (pos >= tokens.length) {
+    return { type: 'tree', nodes: nodes };
   }
 
   var token = tokens[pos];
-  if (token.type === 'text') {
-    var node = _extends({}, token);
-    return createNodeList(tokens, pos + 1, nodes.concat([node]), currentTag);
-  }
   if (token.type === 'open-tag') {
-    var children = createNodeList(tokens, pos + 1, [], token.name);
-
     var name = token.name;
     var start = token.start;
 
-    var tail = children[children.length - 1];
-    var end = tail ? tail.end : token.end;
-    var _node = { type: 'tag', name: name, children: children, start: start, end: end };
+    var content = createNode(tokens, pos + 1, [], name);
 
-    return createNodeList(tokens, pos + children.length + 2, nodes.concat([_node]), currentTag);
+    if (content) {
+      var tail = content.nodes[content.nodes.length - 1];
+      var end = tail ? tail.end : token.end;
+      var _node = { type: 'tag', name: name, content: content };
+      return createNode(tokens, content.endPosition + 1, nodes.concat([_node]), currentTag);
+    }
   }
   if (token.type === 'close-tag') {
     if (token.name === currentTag) {
-      return nodes;
-    } else {
-      var text = token.text;
-      var _start = token.start;
-      var _end = token.end;
-
-      var _node2 = { type: 'text', text: text, start: _start, end: _end };
-      return createNodeList(tokens, pos + 1, nodes.concat([_node2]), currentTag);
+      return { type: 'content', nodes: nodes, endPosition: pos };
     }
   }
+
+  // render as a text node if other cases failed
+  var node = { type: 'text', text: token.text };
+  return createNode(tokens, pos + 1, nodes.concat([node]), currentTag);
 }
 
 function renderNode(node) {
   var type = node.type;
-  var text = node.text;
-  var name = node.name;
-  var children = node.children;
 
   if (type === 'text') {
-    return text;
-  } else if (type === 'tag') {
-    var content = renderNodeList(children);
-    return '<' + name + '>' + content + '</' + name + '>';
+    return node.text;
+  } else if (node.type === 'tag') {
+    return '<' + node.name + '>' + renderNode(node.content) + '</' + node.name + '>';
+  } else if (type === 'content' || type === 'tree') {
+    return node.nodes.map(renderNode).join('');
   }
-}
-
-function renderNodeList(nodes) {
-  return nodes.map(renderNode).join('');
 }
 
 var tokens = getTokens('\n  [b]some bold text[/b]\n  [i]some italics[/i]\n  [color=red]\n    i am red\n    [color=green]i am green[/color]\n  [/color]\n  [url=http://www.google.com]go to google[/url]\n');
 
-var tree = createNodeList(tokens);
+var tree = createNode(tokens);
 
-var output = renderNodeList(tree);
+var output = renderNode(tree);
 
 // console.log(tokens)
-console.log((0, _util.inspect)(tree, { depth: null }));
-// console.log(output)
+// console.log(inspect(tree, { depth: null }))
+console.log(output);
 
 }());
--- src/index.js
+++ src/index.js
@@ -45,51 +45,45 @@ function getTokens(input, position = 0, tokens = []) {
   }
 }
 
-function createNodeList(tokens, pos = 0, nodes = [], currentTag) {
-  if (pos >= tokens.length - 1) {
-    return nodes
+function createNode(tokens, pos = 0, nodes = [], currentTag) {
+  if (pos >= tokens.length) {
+    return { type: 'tree', nodes }
   }
 
   const token = tokens[pos]
-  if (token.type === 'text') {
-    const node = { ...token }
-    return createNodeList(tokens, pos + 1, nodes.concat([ node ]), currentTag)
-  }
   if (token.type === 'open-tag') {
-    const children = createNodeList(tokens, pos + 1, [], token.name)
-
     const { name, start } = token
-    const tail = children[children.length - 1]
-    const end = tail ? tail.end : token.end
-    const node = { type: 'tag', name, children, start, end }
+    const content = createNode(tokens, pos + 1, [], name)
 
-    return createNodeList(tokens, pos + children.length + 2, nodes.concat([ node ]), currentTag)
+    if (content) {
+      const tail = content.nodes[content.nodes.length - 1]
+      const end = tail ? tail.end : token.end
+      const node = { type: 'tag', name, content }
+      return createNode(tokens, content.endPosition + 1, nodes.concat([ node ]), currentTag)
+    }
   }
   if (token.type === 'close-tag') {
     if (token.name === currentTag) {
-      return nodes
-    }
-    else {
-      const { text, start, end } = token
-      const node = { type: 'text', text, start, end }
-      return createNodeList(tokens, pos + 1, nodes.concat([ node ]), currentTag)
+      return { type: 'content', nodes, endPosition: pos }
     }
   }
+
+  // render as a text node if other cases failed
+  const node = { type: 'text', text: token.text }
+  return createNode(tokens, pos + 1, nodes.concat([ node ]), currentTag)
 }
 
 function renderNode(node) {
-  const { type, text, name, children } = node
+  const { type } = node
   if (type === 'text') {
-    return text
+    return node.text
   }
-  else if (type === 'tag') {
-    const content = renderNodeList(children)
-    return `<${name}>${content}</${name}>`
+  else if (node.type === 'tag') {
+    return `<${node.name}>${renderNode(node.content)}</${node.name}>`
   }
-}
-
-function renderNodeList(nodes) {
-  return nodes.map(renderNode).join('')
+  else if (type === 'content' || type === 'tree') {
+    return node.nodes.map(renderNode).join('')
+  }
 }
 
 const tokens = getTokens(`
@@ -102,10 +96,10 @@ const tokens = getTokens(`
   [url=http://www.google.com]go to google[/url]
 `)
 
-const tree = createNodeList(tokens)
+const tree = createNode(tokens)
 
-const output = renderNodeList(tree)
+const output = renderNode(tree)
 
 // console.log(tokens)
-console.log(inspect(tree, { depth: null }))
-// console.log(output)
+// console.log(inspect(tree, { depth: null }))
+console.log(output)
