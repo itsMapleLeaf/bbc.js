2d895e515fd3819add2c5ab0ddcf9cdbab9f76f0
Kingdaro <kingdaro@gmail.com>
2016-10-02 12:42:28 -0400

    a fully functional proper parser, with some bugs...

--- dist/bbc.js
+++ dist/bbc.js
@@ -1,60 +1,131 @@
-(function (exports) {
+(function () {
 'use strict';
 
-var defaultTags = {
-  b: { start: '<strong>', end: '</strong>' },
-  i: { start: '<em>', end: '</em>' },
-  u: { start: '<u>', end: '</u>' },
-  url: { start: function start(_, url) {
-      return '<a href="' + url + '">';
-    }, end: '</a>' },
-  color: { start: function start(_, color) {
-      return '<span style="color: ' + color + '">';
-    }, end: '</span>' },
-  nobbc: { nobbc: true }
-};
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-function parser() {
-  var tags = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : defaultTags;
+var _util = require('util');
 
-  var nobbcExp = new RegExp(Object.keys(tags).filter(function (tag) {
-    return tags[tag].nobbc;
-  }).map(function (tag) {
-    return '\\[' + tag + '(?:=.*?)?]|\\[\\/' + tag + ']';
-  }).join('|'));
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-  var tagExpressions = {};
-  Object.keys(tags).forEach(function (tag) {
-    tagExpressions[tag] = {
-      startexp: new RegExp('\\[' + tag + '(?:=(.*?))?]', 'i'),
-      endexp: new RegExp('\\[\\/' + tag + ']', 'i')
-    };
-  });
-
-  return function (input) {
-    var regions = input.split(nobbcExp);
-
-    for (var i = 0; i < regions.length; i += 2) {
-      for (var tag in tags) {
-        var _tags$tag = tags[tag];
-        var _tags$tag$start = _tags$tag.start;
-        var start = _tags$tag$start === undefined ? '' : _tags$tag$start;
-        var _tags$tag$end = _tags$tag.end;
-        var end = _tags$tag$end === undefined ? '' : _tags$tag$end;
-        var _tagExpressions$tag = tagExpressions[tag];
-        var startexp = _tagExpressions$tag.startexp;
-        var endexp = _tagExpressions$tag.endexp;
-
-        while (startexp.test(regions[i]) && endexp.test(regions[i])) {
-          regions[i] = regions[i].replace(startexp, start).replace(endexp, end);
-        }
-      }
-    }
-
-    return regions.join('');
+function stickyMatcher(pattern) {
+  var exp = new RegExp(pattern, 'y');
+  return function (input, pos) {
+    exp.lastIndex = pos;
+    return exp.exec(input);
   };
 }
 
-exports.parser = parser;
+function tokenMatcher(type, pattern, predicate) {
+  var getMatch = stickyMatcher(pattern);
+  return function (input, position) {
+    var match = getMatch(input, position);
+    if (match) {
+      var props = predicate.apply(undefined, _toConsumableArray(match.slice(1)));
+      return _extends({
+        type: type,
+        text: match[0],
+        start: position,
+        end: position + match[0].length
+      }, props);
+    }
+  };
+}
 
-}((this.BBC = this.BBC || {})));
+var getOpenTag = tokenMatcher('open-tag', /\[([a-z]+?)(?:=(.+?))?]/i, function (name, attr) {
+  return { name: name, attr: attr };
+});
+var getCloseTag = tokenMatcher('close-tag', /\[\/([a-z]+?)]/i, function (name) {
+  return { name: name };
+});
+var getText = tokenMatcher('text', /(\[*[^\[]+)/i, function () {
+  return {};
+});
+
+function getToken(input, position) {
+  return getOpenTag(input, position) || getCloseTag(input, position) || getText(input, position);
+}
+
+function getTokens(input) {
+  var position = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  var tokens = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+
+  if (position >= input.length - 1) {
+    return tokens;
+  }
+  var token = getToken(input, position);
+  if (token) {
+    return getTokens(input, token.end, tokens.concat([token]));
+  } else {
+    throw new Error('we fucked up');
+  }
+}
+
+function createNodeList(tokens) {
+  var pos = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  var nodes = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+  var currentTag = arguments[3];
+
+  if (pos >= tokens.length - 1) {
+    return nodes;
+  }
+
+  var token = tokens[pos];
+  if (token.type === 'text') {
+    var node = _extends({}, token);
+    return createNodeList(tokens, pos + 1, nodes.concat([node]), currentTag);
+  }
+  if (token.type === 'open-tag') {
+    var children = createNodeList(tokens, pos + 1, [], token.name);
+
+    var name = token.name;
+    var start = token.start;
+
+    var tail = children[children.length - 1];
+    var end = tail ? tail.end : token.end;
+    var _node = { type: 'tag', name: name, children: children, start: start, end: end };
+
+    return createNodeList(tokens, pos + children.length + 2, nodes.concat([_node]), currentTag);
+  }
+  if (token.type === 'close-tag') {
+    if (token.name === currentTag) {
+      return nodes;
+    } else {
+      var text = token.text;
+      var _start = token.start;
+      var _end = token.end;
+
+      var _node2 = { type: 'text', text: text, start: _start, end: _end };
+      return createNodeList(tokens, pos + 1, nodes.concat([_node2]), currentTag);
+    }
+  }
+}
+
+function renderNode(node) {
+  var type = node.type;
+  var text = node.text;
+  var name = node.name;
+  var children = node.children;
+
+  if (type === 'text') {
+    return text;
+  } else if (type === 'tag') {
+    var content = renderNodeList(children);
+    return '<' + name + '>' + content + '</' + name + '>';
+  }
+}
+
+function renderNodeList(nodes) {
+  return nodes.map(renderNode).join('');
+}
+
+var tokens = getTokens('\n  [b]some bold text[/b]\n  [i]some italics[/i]\n  [color=red]\n    i am red\n    [color=green]i am green[/color]\n  [/color]\n  [url=http://www.google.com]go to google[/url]\n');
+
+var tree = createNodeList(tokens);
+
+var output = renderNodeList(tree);
+
+// console.log(tokens)
+console.log((0, _util.inspect)(tree, { depth: null }));
+// console.log(output)
+
+}());
--- src/index.js
+++ src/index.js
@@ -1,49 +1,111 @@
-function parser() {
-  // return new parser function
+import {inspect} from 'util'
+
+function stickyMatcher(pattern) {
+  const exp = new RegExp(pattern, 'y')
+  return (input, pos) => {
+    exp.lastIndex = pos
+    return exp.exec(input)
+  }
 }
 
-export { parser }
+function tokenMatcher(type, pattern, predicate) {
+  const getMatch = stickyMatcher(pattern)
+  return (input, position) => {
+    const match = getMatch(input, position)
+    if (match) {
+      const props = predicate(...match.slice(1))
+      return {
+        type,
+        text: match[0],
+        start: position,
+        end: position + match[0].length,
+        ...props,
+      }
+    }
+  }
+}
 
-// const defaultTags = {
-//   b: { start: '<strong>', end: '</strong>' },
-//   i: { start: '<em>', end: '</em>' },
-//   u: { start: '<u>', end: '</u>' },
-//   url: { start: (_, url) => `<a href="${url}">`, end: '</a>' },
-//   color: { start: (_, color) => `<span style="color: ${color}">`, end: '</span>' },
-//   nobbc: { nobbc: true },
-// }
+const getOpenTag = tokenMatcher('open-tag', /\[([a-z]+?)(?:=(.+?))?]/i, (name, attr) => ({ name, attr }))
+const getCloseTag = tokenMatcher('close-tag', /\[\/([a-z]+?)]/i, name => ({ name }))
+const getText = tokenMatcher('text', /(\[*[^\[]+)/i, () => ({}))
 
-// function parser(tags) {
-//   const nobbcExp = new RegExp(Object.keys(tags)
-//     .filter(tag => tags[tag].nobbc)
-//     .map(tag => `\\[${tag}(?:=.*?)?]|\\[\\/${tag}]`)
-//     .join('|'))
-//
-//   const tagExpressions = {}
-//   Object.keys(tags).forEach(tag => {
-//     tagExpressions[tag] = {
-//       startexp: new RegExp(`\\[${tag}(?:=(.*?))?]`, 'i'),
-//       endexp: new RegExp(`\\[\\/${tag}]`, 'i'),
-//     }
-//   })
-//
-//   return input => {
-//     const regions = input.split(nobbcExp)
-//
-//     for (let i = 0; i < regions.length; i += 2) {
-//       for (const tag in tags) {
-//         const { start = '', end = '' } = tags[tag]
-//         const { startexp, endexp } = tagExpressions[tag]
-//         while (startexp.test(regions[i]) && endexp.test(regions[i])) {
-//           regions[i] = regions[i]
-//             .replace(startexp, start)
-//             .replace(endexp, end)
-//         }
-//       }
-//     }
-//
-//     return regions.join('')
-//   }
-// }
-//
-// export { parser }
+function getToken(input, position) {
+  return getOpenTag(input, position) || getCloseTag(input, position) || getText(input, position)
+}
+
+function getTokens(input, position = 0, tokens = []) {
+  if (position >= input.length - 1) {
+    return tokens
+  }
+  const token = getToken(input, position)
+  if (token) {
+    return getTokens(input, token.end, tokens.concat([token]))
+  } else {
+    throw new Error('we fucked up')
+  }
+}
+
+function createNodeList(tokens, pos = 0, nodes = [], currentTag) {
+  if (pos >= tokens.length - 1) {
+    return nodes
+  }
+
+  const token = tokens[pos]
+  if (token.type === 'text') {
+    const node = { ...token }
+    return createNodeList(tokens, pos + 1, nodes.concat([ node ]), currentTag)
+  }
+  if (token.type === 'open-tag') {
+    const children = createNodeList(tokens, pos + 1, [], token.name)
+
+    const { name, start } = token
+    const tail = children[children.length - 1]
+    const end = tail ? tail.end : token.end
+    const node = { type: 'tag', name, children, start, end }
+
+    return createNodeList(tokens, pos + children.length + 2, nodes.concat([ node ]), currentTag)
+  }
+  if (token.type === 'close-tag') {
+    if (token.name === currentTag) {
+      return nodes
+    }
+    else {
+      const { text, start, end } = token
+      const node = { type: 'text', text, start, end }
+      return createNodeList(tokens, pos + 1, nodes.concat([ node ]), currentTag)
+    }
+  }
+}
+
+function renderNode(node) {
+  const { type, text, name, children } = node
+  if (type === 'text') {
+    return text
+  }
+  else if (type === 'tag') {
+    const content = renderNodeList(children)
+    return `<${name}>${content}</${name}>`
+  }
+}
+
+function renderNodeList(nodes) {
+  return nodes.map(renderNode).join('')
+}
+
+const tokens = getTokens(`
+  [b]some bold text[/b]
+  [i]some italics[/i]
+  [color=red]
+    i am red
+    [color=green]i am green[/color]
+  [/color]
+  [url=http://www.google.com]go to google[/url]
+`)
+
+const tree = createNodeList(tokens)
+
+const output = renderNodeList(tree)
+
+// console.log(tokens)
+console.log(inspect(tree, { depth: null }))
+// console.log(output)
